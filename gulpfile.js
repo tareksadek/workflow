9eed6dff9d8c6c332615213cde448938a65054b4
Tarek Sadek <t2madrid@gmail.com>
2018-02-28 20:33:08 +0200

    copy htl from source task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
 var SOURCEPATHS = {
-		sassSource: 'src/scss/*.scss'
+		sassSource: 'src/scss/*.scss',
+		htmlSource: 'src/*.html'
 }
 
 var APPPATHS = {
@@ -15,6 +17,7 @@ var APPPATHS = {
 
 gulp.task('sass', function(){
 	return gulp.src(SOURCEPATHS.sassSource)
+	.pipe(autoprefixer('last 2 versions'))
 	.pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
 	.pipe(gulp.dest(APPPATHS.css));
 });
@@ -27,8 +30,14 @@ gulp.task('serve',['sass'],function(){
 	})
 });
 
-gulp.task('watch', ['serve','sass'], function(){
+gulp.task('copy',function(){
+	gulp.src(SOURCEPATHS.htmlSource)
+	.pipe(gulp.dest(APPPATHS.root));
+});
+
+gulp.task('watch', ['serve','sass','copy'], function(){
 	gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+	gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 });
 
 gulp.task('default', ['watch']);
