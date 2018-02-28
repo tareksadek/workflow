fc7b9eec358c48452309a2cc2c1ad9d719a2fcc8
Tarek Sadek <t2madrid@gmail.com>
2018-02-28 20:02:22 +0200

    browserSync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var SOURCEPATHS = {
+		sassSource: 'src/scss/*.scss'
+}
+
+var APPPATHS = {
+		root: 'app/',
+		css: 'app/css',
+		js: 'app/js'
+}
 
 gulp.task('sass', function(){
-	return gulp.src('src/scss/app.scss')
+	return gulp.src(SOURCEPATHS.sassSource)
 	.pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
-	.pipe(gulp.dest('app/css'));
+	.pipe(gulp.dest(APPPATHS.css));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve',['sass'],function(){
+	browserSync.init([APPPATHS.css + '/*.css', APPPATHS.root + '/*.html', APPPATHS.js + '/*.js'], {
+		server: {
+			baseDir: APPPATHS.root
+		}
+	})
+});
+
+gulp.task('watch', ['serve','sass'], function(){
+	gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+});
+
+gulp.task('default', ['watch']);
